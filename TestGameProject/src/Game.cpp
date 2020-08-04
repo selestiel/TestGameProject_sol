5a201b8076e15c5e7abd72bbfa74539c4a55a380
Selestiel <selarch2018@gmail.com>
2020-08-04 21:21:40 +0300

    player resize

--- TestGameProject/src/Game.cpp
+++ TestGameProject/src/Game.cpp
@@ -31,32 +31,33 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	if (fullscreen == true) {
 		flags = SDL_WINDOW_FULLSCREEN;
 	}
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) 
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer) 
+		if (renderer)
 		{
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		}
 		isRunning = true;
 	}
-	else 
+	else
 	{
 		isRunning = false;
 	}
 
 	map = new Map();
-	
-	player.addComponent<TransformComponent>(2);
+
+	player.addComponent<TransformComponent>(3);
 	player.addComponent<SpriteComponent>("assets/player.png");
 	player.addComponent<KeyboardController>();
 	player.addComponent<ColliderComponent>("player");
 
-	wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20,1);
+	wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
 	wall.addComponent<SpriteComponent>("assets/water.png");
 	wall.addComponent<ColliderComponent>("wall");
-	//SDL_Color white = { 255,255,255,255 };
+	
+	
 }
 void Game::handleEvents() 
 {
@@ -79,6 +80,7 @@ void Game::update()
 	manager.draw();
 	if (Collision::AABB(player.getComponent<ColliderComponent>().collider, wall.getComponent<ColliderComponent>().collider))
 	{
+		player.getComponent<TransformComponent>().scale = 1;
 		std::cout << "Wall hit" << std::endl;
 	}
 
@@ -86,10 +88,10 @@ void Game::update()
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-
+	
 	map->DrawMap();
-	wall.draw();
 	player.draw();
+	wall.draw();
 
 	SDL_RenderPresent(renderer);
 
